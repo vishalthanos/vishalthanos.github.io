60ee7f93c3b2c13413137aa03dbfe03897f19f9d
vishalthanos <p22mcd1010@cit.ac.in>
2022-11-30 03:51:03 +0530

    done

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="bg-gray-600 min-h-screen">
-    <img src="/resume.jpg" alt="vishal-resume">
+    <img src="/resume.jpg" alt="vishal-resume" />
   </div>
 </template>
+<script>
+export default {
+  name: "ResumeView",
+};
+</script>
