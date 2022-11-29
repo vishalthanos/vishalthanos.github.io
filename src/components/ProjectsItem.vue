5b07f02b4c2299b814391927ec19b21db60d91e8
vishalthanos <p22mcd1010@cit.ac.in>
2022-11-29 16:17:24 +0530

    ProjectsItem

--- src/components/ProjectsItem.vue
+++ src/components/ProjectsItem.vue
@@ -29,12 +29,32 @@
 
 <script>
 import { RouterLink } from "vue-router";
-import projects from "../../lib/projectdata";
 export default {
   name: "ProjectItem",
   data() {
     return {
-      projects,
+      projects: [
+        {
+          id: 1,
+          name: "Branding on Mahua",
+          description:
+            "Design a brand identity to uplift a group indegenously grown and manufactured product range ...",
+          img: "/src/assets/images/Mahua-logo.png",
+          link: "/projects/branding-on-mahua",
+        },
+        {
+          id: 2,
+          name: "Typography",
+          description: "Project 2 description",
+          img: "https://picsum.photos/500/300",
+        },
+        {
+          id: 3,
+          name: "Project 3",
+          description: "Project 3 description",
+          img: "https://picsum.photos/500/300",
+        },
+      ],
     };
   },
   components: { RouterLink },
