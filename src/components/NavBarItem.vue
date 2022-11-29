4ef6df6bc4e81423cc92009ef44d4883816b3448
vishalthanos <p22mcd1010@cit.ac.in>
2022-11-29 09:19:08 +0530

    added tailiwnd

---
+++ src/components/NavBarItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class=" font-quick text-lg bg-[#F3EDE9] p-4">
+    <div class="container max-w-xl mx-auto">
+      <nav class="flex text-[#8F7B6A] justify-between items-center">
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/work">Work</RouterLink>
+        <RouterLink to="/resume">Resume</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    name: "NavBarItem",
+    components: { RouterLink }
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
-<script setup>
-import TheWelcome from "../components/TheWelcome.vue";
+<template>
+  <section class="bg-[#464848]">
+    <div
+      class="container  min-h-screen flex animate-in fade-in slide-in-from-right-full delay-500 duration-500 flex-col justify-center items-center gap-8 p-10 sm:flex-row"
+    >
+      <div class="flex">
+        <img src="../assets/images/DP.png" alt="Vishal" />
+      </div>
+      <div class="container mx-auto justify-center items-center flex flex-col">
+        <h1 class="text-3xl">Hi, I am Vishal</h1>
+        <p>
+          I am a designer and a maker, currently pursuing my Master's programme
+          from Central institude of technology kokrajhar
+        </p>
+        <p>
+          I am passionate about designing experiences both from my heart & mind
+          with focus on Design research, Service Design &User Experience Design
+        </p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animate: false,
+    };
+  },
+  mounted() {
+    this.animate = true;
+  },
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style></style>
