b40a770936b789d9691a88fa569d5609de9d20c6
vishalthanos <p22mcd1010@cit.ac.in>
2022-11-29 16:11:15 +0530

    ProjectsItem

---
+++ src/components/ProjectsItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <section
+    v-for="item in projects"
+    :key="item.id"
+    class="min-h-screen bg-[#F3EDE9] flex justify-center text-gray-800 items-center"
+  >
+    <div class="bg-[#858585] m-20 w-[80vw] h-[80vh] flex relative">
+      <div
+        class="bg-[#F3EDE9] absolute flex items-center bottom-20 py-20 shadow top-20 p-20 left-64 w-[400px] z-[1]"
+      >
+        <div class="w-56">
+          <h2 class="text-2xl">{{ item.name }}</h2>
+          <p class="mt-10 text-sm text-gray-700">{{ item.description }}</p>
+          <RouterLink
+            :to="'/projects/' + $(item.id)"
+            class="absolute items-center hover:underline bottom-10 right-10 mt-10 rounded-md"
+            >View Project &rarr;</RouterLink
+          >
+        </div>
+      </div>
+      <div
+        class="bg-slate-50 absolute shadow flex justify-center items-center w-[400px] top-10 right-48 p-8 z-[2]"
+      >
+        <img class="border-2 border-black" :src="item.img" :alt="item.name" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { RouterLink } from "vue-router";
+import projects from "../../lib/projectdata";
+export default {
+  name: "ProjectItem",
+  data() {
+    return {
+      projects,
+    };
+  },
+  components: { RouterLink },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,21 @@
 <template>
-  <section class="bg-hero-gradient relative px-20">
+  <section class="bg-hero-gradient relative px-20 overflow-hidden">
     <!-- <div class="flex -z-10">
       <img src="../assets/images/DP.png" alt="Vishal" />
     </div> -->
     <div
-      class="container min-h-screen flex animate-in fade-in slide-in-from-right-full duration-1000 flex-col justify-between items-center gap-10 p-10 sm:flex-row"
+      class="container min-h-[90vh] flex animate-in fade-in slide-in-from-right-full duration-1000 flex-col justify-between items-center gap-10 p-10 sm:flex-row"
     >
       <div class="flex">
         <img src="../assets/images/DP.png" alt="Vishal" />
       </div>
-      <div class="container max-w-xl items-center flex flex-col h-36">
-        <h1 class="text-4xl font-inter text-[#8F7B6A]">Hi, I am Vishal</h1>
-        <div class="font-quick text-[#4A4A4A] relative">
+      <div class="container max-w-xl flex flex-col h-36">
+        <h1
+          class="text-4xl font-inter text-[#8F7B6A] animate-in fade-in-0 slide-in-from-top-full delay-1000 duration-700"
+        >
+          Hi, I am Vishal
+        </h1>
+        <div class="font-quick text-[#4A4A4A]">
           <p class="mt-6">
             I am a designer and a maker, currently pursuing my Master's
             programme from Central institude of technology kokrajhar
@@ -21,17 +25,39 @@
             mind with focus on Design research, Service Design &User Experience
             Design
           </p>
-          <div class="absolute bottom-0">
-            <img src="../assets/images/Collage.png" alt="vishal-college" />
+          <div class="absolute bottom-28">
+            <img
+              class="h-10"
+              src="../assets/images/Collage.png"
+              alt="vishal-college"
+            />
+          </div>
+        </div>
+
+        <div
+          class="bg-gray-300 w-36 h-36 absolute -bottom-[50px] transform -translate-x-1/2 flex justify-center items-center left-1/2 rounded-full border border-gray-400 border-dashed"
+        >
+          <div
+            class="bg-gray-50 w-20 text-gray-500 h-20 rounded-full flex justify-center items-center flex-col"
+          >
+            <div
+              class="animate-bounce flex justify-center flex-col items-center"
+            >
+              &darr;
+              <p class="text-xs">Scroll Down</p>
+            </div>
           </div>
         </div>
       </div>
     </div>
   </section>
+  <projects-item />
 </template>
 
 <script>
+import ProjectsItem from "../components/ProjectsItem.vue";
 export default {
+  components: { ProjectsItem },
   data() {
     return {
       animate: false,
